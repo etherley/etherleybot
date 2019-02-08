251c365ad6ec2e03395204acb60a4724de43cc84
Gus <yo@soygus.com>
2019-02-08 10:58:58 -0600

    Adds bodyParser to express config

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,20 @@ require('dotenv').config()
 
 import app from './app';
 import { EtherleyBot } from './bot';
+import bodyParser = require('body-parser');
 
-const PORT = 3000
 const BOT = new EtherleyBot(process.env.BOT_TOKEN)
 
-app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT)
-    BOT.init()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/stripe', function (req, res) {
+  console.log(req.body)
+  res.sendStatus(200)
 })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Express server listening on port ${process.env.PORT}`)
+  BOT.init()
+})
+
