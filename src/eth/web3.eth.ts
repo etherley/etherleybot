09720425a119ec2423ee7f09183b281143eea9e0
Gus <yo@soygus.com>
2019-02-19 10:07:25 -0600

    Types Web3Provider utils and eth

--- src/eth/web3.eth.ts
+++ src/eth/web3.eth.ts
@@ -1,13 +1,22 @@
 import Web3 from 'web3';
+import { Utils } from 'web3-utils';
+import { Eth } from 'web3-eth';
 
+export interface Network {
+  mainnet: string,
+  ropsten: string,
+  local: string,
+}
 export default class Web3Provider {
 
-  utils: any
+  utils: Utils
+  eth: Eth
   instance: Web3
 
-  constructor() {
-    const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/3ffb5509990b4964acd6de7991d5750d')
+  constructor(network: string = process.env.ETHEREUM_NETWORK) {
+    const provider = new Web3.providers.HttpProvider(network)
     this.instance = new Web3(provider)
     this.utils = this.instance.utils
+    this.eth = this.instance.eth
   }
 }
